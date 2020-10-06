3cf5292b2e9215954c89f434dc6f695b42a9e130
Joe Gracey <joe.gracey@gmail.com>
2020-10-06 17:49:46 -0600

    figure out test

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,34 +2,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-class MainTest {
+
+
+class TestIntroMessage {
 
     @Test
-    void main() {
-    }
+    public void testPleaseEnterTheFollowing() {
 
-    @Test
-    void testIntroMessage() {
 
-    }
 
-    @Test
-    void testMonthlySalary() {
-    }
-
-    @Test
-    void testOtherIncome() {
-    }
-
-    @Test
-    void testBudgetedLivingExpenses() {
-    }
-
-    @Test
-    void testTitheOfferings() {
-    }
-
-    @Test
-    void testOtherExpenses() {
     }
 }
